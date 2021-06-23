66901e58a1f5fcf2121e89f4615e28f185037d88
Accurics <admin@accurics.com>
2021-06-23 15:49:59 -0400

    Auto Remediation By Accurics

--- terraform/modules/ecs-fargate/main.tf
+++ terraform/modules/ecs-fargate/main.tf
@@ -250,7 +250,7 @@ resource "aws_security_group" "alb" {
     from_port   = 443
     to_port     = 443
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["<cidr>"]
   }
 
   egress {
